1473286ba42480ae254c83ad36282e69c2278bdb
Cole Flournoy <flournoy.cole@gmail.com>
2021-03-11 17:27:02 -0500

    Add #display_allergies

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ApplicationRecord
   belongs_to :classroom
   has_many :severities
   has_many :allergies, through: :severities
+
+  def display_allergies
+    self.severities.map{|s| "Allergic to #{s.allergy.name} and #{s.name}"}
+  end
 end
