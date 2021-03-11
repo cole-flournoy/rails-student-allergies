230f2d5498c5d84371a08104343cc4f949c7f532
Cole Flournoy <flournoy.cole@gmail.com>
2021-03-11 15:14:48 -0500

    Stub allergy CRUD actions and views

--- app/controllers/allergies_controller.rb
+++ app/controllers/allergies_controller.rb
@@ -1,2 +1,52 @@
 class AllergiesController < ApplicationController
+  def index
+    @allergies = Allergy.all
+  end
+
+  def show
+    @allergy = Allergy.find(params[:id])
+  end
+
+  def new
+    @allergy = Allergy.new
+  end
+
+  def create
+    @allergy = Allergy.new(allergy_params)
+
+    if @allergy.save
+      redirect_to @allergy
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @allergy = Allergy.find(params[:id])
+  end
+
+  def update
+    @allergy = Allergy.find(params[:id])
+
+    @allergy.update(allergy_params)
+
+    if @allergy.save
+      redirect_to @allergy
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @allergy = Allergy.find(params[:id])
+    @allergy.destroy
+    # flash[:notice] = "Allergy deleted."
+    redirect_to allergies_path
+  end
+
+  private
+
+  def allergy_params
+    params.require(:allergy).permit(:name)
+  end
 end
