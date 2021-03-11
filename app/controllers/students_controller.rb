a3db3724356324c9d5835a8a36a56275d02be68d
Cole Flournoy <flournoy.cole@gmail.com>
2021-03-11 14:33:38 -0500

    Stub student CRUD actions and views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,52 @@
 class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      redirect_to @student
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+
+    @student.update(student_params)
+
+    if @student.save
+      redirect_to @student
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    # flash[:notice] = "Student deleted."
+    redirect_to students_path
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :classroom_id)
+  end
 end
