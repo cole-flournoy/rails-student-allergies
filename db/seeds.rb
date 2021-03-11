217aa87c1212d2d23fea5bf611278b7e45c2bef5
Cole Flournoy <flournoy.cole@gmail.com>
2021-03-10 23:13:00 -0500

    Add initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+bob = User.create(name: "Mr. Bob")
+bob_class = Classroom.create(name: "Mr. Bob's Class", user: bob)
+
+jimmy = Student.create(name: "Jimmy", classroom: bob_class)
+timmy = Student.create(name: "Timmy", classroom: bob_class)
+mimmy = Student.create(name: "Mimmy", classroom: bob_class)
+bimmy = Student.create(name: "Bimmy", classroom: bob_class)
+
+pnuts = Allergy.create(name: "Peanuts")
+weird = Allergy.create(name: "Eye contact")
+
+
+epi = Severity.create(name: "Needs Epinephrine", student: jimmy, allergy: pnuts)
+mild = Severity.create(name: "Needs to be left alone", student: mimmy, allergy:weird)
+
+epi2 = Severity.create(name: "Needs Epinephrine", student: timmy, allergy: pnuts)
+mild2 = Severity.create(name: "Needs to be left alone", student: timmy, allergy:weird)
