1e4fddfbf015ed3dcf5b4e9ac74807fdf6d6b7a2
Cole Flournoy <flournoy.cole@gmail.com>
2021-03-12 22:58:33 -0500

    Add allergy_count helper for Classrooms

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,4 +1,10 @@
 class Classroom < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :students
+
+  def allergy_count
+    count = 0
+    self.students.each{|s| count += s.allergies.count}
+    count
+  end
 end
